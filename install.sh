f4690b4408ea2e9bd5a3567919a6db4764ddb1c6
Đồng Sơn Tùng <tungdsse04019@fpt.edu.vn>
2018-01-15 22:24:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp laravel.tpl /usr/local/vesta/data/templates/web/nginx/php5-fpm/laravel.tpl
-cp laravel.stpl /usr/local/vesta/data/templates/web/nginx/php5-fpm/laravel.stpl
+cp laravel.tpl /usr/local/vesta/data/templates/web/nginx/php-fpm/laravel.tpl
+cp laravel.stpl /usr/local/vesta/data/templates/web/nginx/php-fpm/laravel.stpl
 
 cp fastcgi_params /etc/nginx/fastcgi_params
 
